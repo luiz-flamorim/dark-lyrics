b2ee147924da8c96dbe2599bdfd499ec3c2ccc90
luiz-flamorim <luiz.flamorim@gmail.com>
2021-02-16 23:24:53 +0000

    testing Json response

--- pageScraper.js
+++ pageScraper.js
@@ -13,12 +13,12 @@ const scraperObject = {
         let allUrl = []
         let bands = []
 
-        for (let i = 0; i < 10; i++) { //alphabetList.length/2
+        for (let i = 0; i < alphabetList.length/2; i++) { 
 
             let allArtistsPage = await browser.newPage()
             await allArtistsPage.setDefaultNavigationTimeout(0)
 
-            console.log(`Scrapping page ${alphabetList[i]}`);
+            console.log(`index ${i} | Scrapping page ${alphabetList[i]}`);
             await allArtistsPage.goto(alphabetList[i]);
 
             let artistList2 = await allArtistsPage.$$eval('div.artists.fr a', urls => urls.map(url1 => url1.href))
@@ -33,64 +33,56 @@ const scraperObject = {
         allUrl = allUrl.flat().filter(item => ~item.indexOf("darklyrics"));
         //I have filtered links not matching to darlyrics.com: 8973 original links to 8734 remaining
 
-        for (let i = 0; i < 10; i++) { //allUrl.length
+        for (let i = allUrl.length; i < allUrl.length; i++) {
             let artistPage = await browser.newPage()
             await artistPage.setDefaultNavigationTimeout(0)
 
-            console.log(`Scrapping the band page ${allUrl[i]}`)
+            console.log(`index ${i} | Scrapping the band page ${allUrl[i]}`)
             await artistPage.goto(allUrl[i]);
 
             let bandName = await artistPage.$$eval('div.cont h1', band => band.map(name => name.textContent))
 
             let albumData = await artistPage.$$eval('div.album', albums => albums.map(album => {
-                const nameAndYear = album.querySelector('h2').textContent;
-                const albumName = album.querySelector('h2 strong').textContent;
+
+                let nameAndYear = album.querySelector('h2').textContent
+                if (!nameAndYear.includes('(')) {
+                    nameAndYear += '(no data)'
+                }
+
+                let albumName
+                if (album.querySelector('h2 strong')) {
+                    albumName = album.querySelector('h2 strong').textContent;
+                } else {
+                    albumName = album.querySelector('h2').textContent;
+                }
 
                 const aSongs = Array.from(album.querySelectorAll('a')).map(song => {
                     const songName = song.textContent
                     const songUrl = song.href
-
                     return {
                         songName: songName,
                         songUrl: songUrl
                     }
                 })
-
                 return {
-                    albumName: albumName.replaceAll('\"',''),
-                    albumYear: nameAndYear.match(/\(([^\)]+)\)/).slice(1, 2),
+                    albumName: albumName.replaceAll('\"', ''),
+                    albumYear: nameAndYear.match(/\(([^\)]+)\)/).slice(1, 2)[0],
                     albumSongs: aSongs,
                 }
             }))
-
             let band = new Object({
-                url: allUrl[i],
-                name: bandName[0].replace(' LYRICS', ''),
-                albums: albumData
+                bandUrl: allUrl[i],
+                bandName: bandName[0].replace(' LYRICS', ''),
+                bandAlbums: albumData
             })
-
             bands.push(band)
         }
-
-
         const fs = require('fs');
-        fs.writeFileSync('./results.json', JSON.stringify(bands));
+        fs.writeFileSync('./results.json', JSON.stringify(bands, null, '\t'));
+        // console.log(JSON.stringify(bands, null, '\t'))
 
         await browser.close();
-
     }
 }
 
 module.exports = scraperObject;
-
-//-------backup!
-
-// for (a in albumData) {
-//     for (b in albumData[a].aSongs) {
-//         console.log(albumData[a].aSongs[b])
-//     }
-// }
-
-// for(a in bands){
-//     console.log(bands[a])
-// }
