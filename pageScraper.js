b35ff0c238483ae79dbbd272ed9c13d7a552b0c0
luiz-flamorim <luiz.flamorim@gmail.com>
2021-02-18 20:26:10 +0000

    now closing the browser window - I ended up with 4000 open tabs!

--- pageScraper.js
+++ pageScraper.js
@@ -1,54 +1,55 @@
 const scraperObject = {
     url: 'http://www.darklyrics.com',
     async scraper(browser) {
-        let page = await browser.newPage();
-        await page.setDefaultNavigationTimeout(0)
-
-        console.log(`Opening ${this.url}`);
-        await page.goto(this.url);
-        let alphabetList = await page.$$eval('div.listrow a', urls => urls.map(url1 => url1.href))
-
-        page.close()
 
         let allUrl = []
         let bands = []
-        let errors = []
 
+        let page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0)
+        //Collecting the alphabet list of links
+        console.log(`Opening ${this.url}`);
+        await page.goto(this.url);
+        let alphabetList = await page.$$eval('div.listrow a', urls => urls.map(url1 => url1.href))
+        page.close()
+
+        // loop through the alphavet list to collect all the band links - there are 2 elements, that's why the lenght '/2'
         for (let i = 0; i < alphabetList.length / 2; i++) {
-        // for (let i = 0; i < 1; i++) {
             let allArtistsPage = await browser.newPage()
             await allArtistsPage.setDefaultNavigationTimeout(0)
 
+            // Image and CSS request interceptors - blocking them to improve the performance
+            page.on('request', request => {
+                if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet')
+                    request.abort();
+                else
+                    request.continue();
+            });
             console.log(`index ${i} | Scrapping page ${alphabetList[i]}`);
             await allArtistsPage.goto(alphabetList[i]);
-
             let artistList2 = await allArtistsPage.$$eval('div.artists.fr a', urls => urls.map(url1 => url1.href))
             let artistList1 = await allArtistsPage.$$eval('div.artists.fl a', urls => urls.map(url1 => url1.href))
             let artistsUrlList = artistList1.concat(artistList2)
-
             allUrl.push(artistsUrlList)
 
             allArtistsPage.close()
         }
 
+        //Filter the band links to darklyrics only: from 8973 original links to 8734 remaining
         allUrl = allUrl.flat().filter(item => ~item.indexOf("darklyrics"));
-        //I have filtered links not matching to darlyrics.com: 8973 original links to 8734 remaining
 
+        //loop thrugh the band links, returnng a band element with all the information inside
         for (let i = 0; i < allUrl.length; i++) {
-        // for (let i = 0; i < 1; i++) {
             let artistPage = await browser.newPage()
             await artistPage.setDefaultNavigationTimeout(0)
 
             console.log(`index ${i} | Scrapping the band page ${allUrl[i]}`)
             await artistPage.goto(allUrl[i]);
 
-            //Checking if there is content in the page
-            // let isPageValid = await artistPage.$$eval('div.cont h2', band => band.map(name => name.textContent))
-            // if (!isPageValid[0].includes('not found')) {
+            //try/ catch as some of the links will throw errors
             try {
 
                 let bandName = await artistPage.$$eval('div.cont h1', band => band.map(name => name.textContent))
-
                 let albumData = await artistPage.$$eval('div.album', albums => albums.map(album => {
 
                     let nameAndYear = album.querySelector('h2').textContent
@@ -57,28 +58,18 @@ const scraperObject = {
                     }
 
                     let albumName;
-                    let albumType
                     try {
                         albumName = album.querySelector('h2 strong').textContent
-                        // let albumName
-                        // if (album.querySelector('h2 strong')) {
-                        //     albumName = album.querySelector('h2 strong').textContent;
-                        // } else {
-                        //     albumName = album.querySelector('h2').textContent;
-                        // }
-
-                        albumType = album.textContent.split(':')[0]
-                        // let albumType
-                        // if(album.textContent.includes(':')){
-                        //     albumType = album.textContent.split(':')[0]
-                        // } else {
-                        //     albumType = '(no data)'
-                        // }
                     } catch (error) {
                         albumName = album.querySelector('h2').textContent;
-                        albumType = '(no data)'
                     }
 
+                    let albumType
+                    try {
+                        albumType = album.textContent.split(':')[0]
+                    } catch (error) {
+                        albumType = '(no data)'
+                    }
 
                     const aSongs = Array.from(album.querySelectorAll('a')).map(song => {
                         const songName = song.textContent
@@ -101,21 +92,23 @@ const scraperObject = {
                     bandAlbums: albumData
                 })
                 bands.push(band)
+                artistPage.close()
+
             } catch (error) {
+                //collecting errors as well in the same band element
                 console.log('Error: ' + allUrl[i])
                 let band = new Object({
                     bandUrl: allUrl[i],
                     error: error
                 })
                 bands.push(band)
+                artistPage.close()
             }
+            //writing the results to JSON
             const fs = require('fs');
             fs.writeFileSync('./results.json', JSON.stringify(bands, null, '\t'));
-            // fs.writeFileSync('./errors.json', JSON.stringify(errors, null, '\t'));
-            // console.log(JSON.stringify(bands, null, '\t'))
         }
         await browser.close();
     }
 }
-
 module.exports = scraperObject;
