bd1fd1b53ef9da0a4791f6f44f473a3f88c945d6
luiz-flamorim <luiz.flamorim@gmail.com>
2021-02-25 20:09:56 +0000

    trying to make the chart responsive

--- BarChart/new-bar-chart.js
+++ BarChart/new-bar-chart.js
@@ -1,104 +1,99 @@
-// define margins
-let unit = 10
-let animationSpeed = 1000;
 
-let margin = {
-    top: unit*3,
-    right: unit*5,
-    bottom: unit*15,
-    left: unit*5,
-}
+    let chartWidth = document.querySelector(".glass").offsetWidth
+    let chartHeight = document.querySelector(".glass").offsetHeight
+    let margin = {
+        top: 20,
+        right: 25,
+        bottom: 30,
+        left: 25,
+    }
+    let width = chartWidth - margin.left - margin.right;
+    let height = chartHeight - margin.top - margin.bottom;
 
-let width = unit * 90 - margin.left - margin.right;
-let height = unit * 50 - margin.top - margin.bottom;
-//Q: how to make this relative to the CSS styles?
-
-// add the properties to the SVG
-let svg = d3.select('#chart1')
-    .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
- //Create the scales - fixed values
-let x = d3.scaleBand()
-    .range([0, width])
-    .padding(0.15)
-
-let y = d3.scaleLinear()
-    .range([height, 0])
-
-// parse the data 
 d3.json('/Scrapper/results.json')
-    .then((data) => {
-
-        // function parses the data and returns an array of Key and Value
-        let entries = initialLetters(data)
-
-        const maxCount = d3.max(entries, d => d.value);
-        let letters = entries.map(d => d.key);
-        letters = letters.sort();
-
-        //add domain to scales: values that will vary
-        x.domain(letters)
-        y.domain([0, maxCount])
-            .nice() // rounds up the value for the axis
-
-        svg.append('g') // y axis
-            .call(d3.axisLeft(y))
-            .attr('class','text-axis')
-
-        svg.append('g') // x axis
-            .attr('transform', `translate(0, ${height})`)
-            .call(d3.axisBottom(x))
-            .selectAll('text')
-            .attr('class','text-axis')
-            .attr('text-anchor', 'middle')
-
-        // function uses the parsed data to iterate through and create the bar chart
-        createBars(entries)
-
-    }).catch((error) => {
+    .then(data => buildChart(data, width, height))
+    .catch((error) => {
         throw error;
+    });
+
+    window.addEventListener('resize', (e) => {
+        chartWidth = document.querySelector(".glass").offsetWidth
+        buildChart(data, width, height)
     })
 
-// FUNCTIONS
-function createBars(data) {
+function buildChart(data, width, height) {
+
+    //Q: how to make this relative to the CSS styles?
+    let animationSpeed = 1000;
+
+    let svg = d3.select('#chart1')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+    //Create the scales - fixed values
+    let x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.15)
+
+    let y = d3.scaleLinear()
+        .range([height, 0])
+
+    let entries = initialLetters(data)
+    let maxCount = d3.max(entries, d => d.value)
+    let letters = entries.map(d => d.key).sort()
+
+    x.domain(letters) //add domain to scales: values that will vary
+    y.domain([0, maxCount])
+        .nice() //rounds up the value for the axis
+
+    svg.append('g') // x axis
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(x))
+        .selectAll('text')
+        .attr('text-anchor', 'middle')
+
     svg.selectAll('.bar-group')
-        .data(data, d => d.key)
-        .join(
-            enter => {
-                let bar = enter.append('g')
-                    .attr('class', 'bar-group')
-                    .style('opacity', 1)
+    .data(entries, d => d.key)
+    .join(enter => {
+        let bar = enter.append('g')
+            .attr('class', 'bar-group')
+            .style('opacity', 1)
 
-                bar.append('rect')
-                    .attr('class', 'bar')
-                    .attr('x', d => x(d.key))
-                    .attr('y', d => y(0))
-                    .attr('width', x.bandwidth())
-                    .attr('height', 0)
-                    .transition()
-                    .duration(animationSpeed)
-                    .attr('y', d => y(d.value))
-                    .attr('height', d => height - y(d.value))
+        bar.append('rect')
+            .attr('class', 'bar')
+            .attr('x', d => x(d.key))
+            .attr('y', d => y(0))
+            .attr('width', x.bandwidth())
+            .attr('height', 0)
+            .transition()
+            .delay(animationSpeed / 10)
+            .duration(animationSpeed)
+            .attr('y', d => y(d.value))
+            .attr('height', d => height - y(d.value))
 
-                bar.append('text')
-                    .text(d => d.value)
-                    .attr('x', d => x(d.key) + (x.bandwidth() / 2))
-                    .attr('y', d => y(d.value) - 5)
-                    .attr('text-anchor', 'middle')
-                    .attr('class','text-bar')
-                    .style('opacity', 0)
-                    .transition()
-                    .delay(animationSpeed/3)
-                    .duration(animationSpeed/5)
-                    .style('opacity', 1)
-            }
-        )
+        bar.append('text')
+            .attr('class', 'textbar')
+            .attr('style', 'color: #fff')
+            .attr('x', d => x(d.key) + (x.bandwidth() / 2))
+            .attr('text-anchor', 'middle')
+            .attr("y", d => {
+                return height;
+            })
+            .attr("height", 0)
+            .style('opacity', 0)
+            .transition()
+            .duration(animationSpeed)
+            .text(d => d.value)
+            .attr('y', d => y(d.value) - 10)
+            .style('opacity', 1)
+    })
+
 }
 
+// FUNCTIONS
 function initialLetters(data) {
     data = data.filter(v => v.bandName != null)
     let bandInitials = []
@@ -124,5 +119,5 @@ function initialLetters(data) {
         key,
         value
     }))
-    return entries;
+    return entries
 }
