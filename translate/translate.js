d3dc372afb09f67ac323af81bcedb07ced249893
luiz-flamorim <luiz.flamorim@gmail.com>
2021-03-11 00:14:33 +0000

    adjustments on the async function

--- translate/translate.js
+++ translate/translate.js
@@ -5,44 +5,40 @@ const {
 const translate = new Translate();
 let text = '';
 const target = 'en';
+
 const fs = require('fs');
 // const data = JSON.parse(fs.readFileSync('/Users/luizamorim/Desktop/darklyrics/Scrapper/results.json', 'utf8'));
 const data = JSON.parse(fs.readFileSync('/Users/luizamorim/Desktop/darklyrics/Scrapper/results_test.json', 'utf8'));
 
-translateToJson(data)
+let originalLanguage = getOriginalTitles(data)
 
-async function translateToJson(data) {
-  let originalLanguage = getOriginalTitles(data)
-  let enSongName =  originalLanguage.forEach(song => {
-  //   let s = song.toString()
-  let result =  translateText(song)
-  
-})
+updateJson()
 
-console.log(result)
-
+async function updateJson(){
+  const traslatedSongs = await translateText(originalLanguage)
+  // traslatedSongs.forEach(song => )
+  console.log(traslatedSongs)
 }
-
-// detectLanguage();
-// translateText();
 
 async function translateText(originalString) {
   let [translations] = await translate.translate(originalString, target);
   translations = Array.isArray(translations) ? translations : [translations];
-  // console.log('Translations:');
+
   translations.forEach((translation, i) => {
     // console.log(`${text} => (${target}) ${translation}`);
-    return translation;
   });
+  // console.log(translations)
+  return translations;
 }
 
-async function detectLanguage() {
-  let [detections] = await translate.detect(text);
+async function detectLanguage(originalString) {
+  let [detections] = await translate.detect(originalString);
   detections = Array.isArray(detections) ? detections : [detections];
   // console.log('Detections:');
   detections.forEach(detection => {
-    console.log(`${detection.input} => ${detection.language}`);
+    // console.log(`${detection.input} => ${detection.language}`);
   });
+  return detections
 }
 
 function getOriginalTitles(data) {
