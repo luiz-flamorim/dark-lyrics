db1c5070ade0ada3bdf462af2f67de8135ab91d2
luiz-flamorim <luiz.flamorim@gmail.com>
2021-03-12 20:06:43 +0000

    google translate api is working!

--- translate/translate.js
+++ translate/translate.js
@@ -1,30 +1,36 @@
-// Imports the Google Cloud client library
 const {
   Translate
 } = require('@google-cloud/translate').v2;
 const translate = new Translate();
-let text = '';
 const target = 'en';
 
 const fs = require('fs');
 // const data = JSON.parse(fs.readFileSync('/Users/luizamorim/Desktop/darklyrics/Scrapper/results.json', 'utf8'));
 const data = JSON.parse(fs.readFileSync('/Users/luizamorim/Desktop/darklyrics/Scrapper/results_test.json', 'utf8'));
 
-let originalLanguage = getOriginalTitles(data)
+getAndTranslate(data)
 
-updateJson()
-
-
-async function updateJson(){
-  const translatedSongs = await translateText(originalLanguage)
-  // console.log(translatedSongs)
-  translatedSongs.forEach(song => console.log(song))
+async function getAndTranslate(data) {
+  for (b in data) {
+    let band = data[b]
+    for (a in band.bandAlbums) {
+      let album = band.bandAlbums[a]
+      for (s in album.albumSongs) {
+        let song = album.albumSongs[s]
+        let translatedSong = await translateText(song.songName)
+        let originalLanguage = await detectLanguage(song.songName)
+        // console.log(translatedSong, [originalLanguage[0].language])
+        song['orignialLanguage'] = originalLanguage
+        song['songEn'] = translatedSong
+        fs.writeFileSync('./results-33.json', JSON.stringify(data, null, '\t'))
+      }
+    }
+  }
 }
 
 async function translateText(originalString) {
   let [translations] = await translate.translate(originalString, target);
   translations = Array.isArray(translations) ? translations : [translations];
-
   translations.forEach((translation, i) => {
     // console.log(`${text} => (${target}) ${translation}`);
   });
@@ -35,30 +41,8 @@ async function translateText(originalString) {
 async function detectLanguage(originalString) {
   let [detections] = await translate.detect(originalString);
   detections = Array.isArray(detections) ? detections : [detections];
-  // console.log('Detections:');
   detections.forEach(detection => {
     // console.log(`${detection.input} => ${detection.language}`);
   });
   return detections
-}
-
-async function getOriginalTitles(data) {
-  let foreignTitles = []
-  data.forEach(band => {
-    band.bandAlbums.forEach(album => {
-      album.albumSongs.forEach(song => {
-        foreignTitles.push(song.songName)
-
-        // create a new attribute for the name in english
-        // translate the song name using the google function
-        // push the new song name to the new attribute
-
-        // create a new attribute for the original language
-        // check the original language using the google function
-        // push the original language to the new atribute
-      });
-    });
-  })
-  // console.log(foreignTitles)
-  return foreignTitles
 }
