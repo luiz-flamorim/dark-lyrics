410fe1f3ef088f2e5649c4d0dfd5aeea6593b78c
luiz-flamorim <luiz.flamorim@gmail.com>
2021-04-10 21:31:27 +0100

    working on the d3.tip()

--- BarChart/timeline.js
+++ BarChart/timeline.js
@@ -4,7 +4,7 @@ const margin = {
     top: 100,
     bottom: 200
 }
-const height = window.innerHeight - margin.top - margin.bottom
+const height = 3000 - margin.top - margin.bottom
 const width = window.innerWidth - margin.left - margin.right
 
 d3.json('/Scrapper/results.json')
@@ -13,9 +13,6 @@ d3.json('/Scrapper/results.json')
     })
 
 function createTimeline(data) {
-
-    // d3.select('#timeline')
-    // .attr('height','5000px')
 
     let yearCount = new Map();
     for (let i = 0; i < data.length; i++) {
@@ -27,16 +24,23 @@ function createTimeline(data) {
             }
         }
     }
+    yearCount.delete('0000')
+    // console.log(yearCount)
+
     const sortedYears = Array.from(yearCount.keys()).sort()
     const sortedValues = Array.from(yearCount.values()).sort()
 
     const scale = d3.scalePoint()
         .domain(sortedYears)
-        .range([height-margin.bottom, 0])
+        .range([height - margin.bottom, 0])
+
+    // const scale = d3.scaleTime()
+    //     .domain(sortedYears)
+    //     .range([height - margin.bottom, 0])
 
     const minMaxScale = d3.scaleLinear()
         .domain([d3.min(sortedValues), d3.max(sortedValues)])
-        .range([10, 100])
+        .range([10, 200])
 
     let svg = d3.select('#timeline')
         .append('div')
@@ -45,7 +49,6 @@ function createTimeline(data) {
         .attr("preserveAspectRatio", "xMinYMin meet")
         .attr("viewBox", `0 0 ${width} ${height}`)
         .classed('svg-content-responsive', true)
-        .style('background-color','red')
         .append('g')
         .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
@@ -58,10 +61,45 @@ function createTimeline(data) {
         .attr('cx', x)
         .attr('cy', d => scale(d[0]))
         .attr('r', d => minMaxScale(d[1]))
+        .style('fill', 'none')
+        .style('stroke', 'white')
+        .style('stroke-width', '1px')
+        .style('stroke-opacity', '0.5')
+        .on("mouseover", mouseOver)
+        .on("mouseout", mouseOut)
 
     circles.join('text')
-        .attr('x', x + 10)
+        .attr('x', x)
         .attr('y', d => scale(d[0]))
         .text(d => d[0])
         .style('fill', '#fff')
+        .style("font-size", 12)
+        .style("font-family", 'Lekton')
+        .style("alignment-baseline", 'central')
+        .style("text-anchor", 'middle')
+
+    const tooltip = d3.select("#timeline")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "black")
+        .style("color", "white")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+
 }
+
+function mouseOver() {
+    d3.select(this)
+        .style('stroke', 'red')
+        .append("title")
+        .text(d => `${d[1]} albums were released in ${d[0]}`)
+}
+
+function mouseOut() {
+    d3.select(this)
+        .style('stroke', 'white')
+}
+
+//tooltip
+// https://github.com/Caged/d3-tip/blob/HEAD/docs/index.md
