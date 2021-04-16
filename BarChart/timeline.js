780e188d53b295c65129664aadb4855ee8bd08d1
luiz-flamorim <luiz.flamorim@gmail.com>
2021-04-16 19:03:47 +0100

    timeline almost there

--- BarChart/timeline.js
+++ BarChart/timeline.js
@@ -14,7 +14,9 @@ d3.json('/Scrapper/results.json')
 
 function createTimeline(data) {
 
-    let yearCount = new Map();
+    let yearCount = new Map()
+    let yearAlbum = new Map()
+
     for (let i = 0; i < data.length; i++) {
         for (let j = 0; j < data[i].bandAlbums.length; j++) {
             if (!yearCount.has(data[i].bandAlbums[j].albumYear)) {
@@ -22,10 +24,16 @@ function createTimeline(data) {
             } else {
                 yearCount.set(`${data[i].bandAlbums[j].albumYear}`, yearCount.get(`${data[i].bandAlbums[j].albumYear}`) + 1)
             }
+            // if (!yearAlbum.has(data[i].bandAlbums[j].albumYear)) {
+            //     yearAlbum.set(`${data[i].bandAlbums[j].albumYear}`, [`${data[i].bandAlbums[j].albumName}`])
+            // } else {
+            //     yearAlbum.set(`${data[i].bandAlbums[j].albumYear}`, yearAlbum.get(`${data[i].bandAlbums[j].albumYear}`).push(`${data[i].bandAlbums[j].albumName}`))
+            // }
         }
     }
+
     yearCount.delete('0000')
-    // console.log(yearCount)
+    // console.log(yearAlbum)
 
     const sortedYears = Array.from(yearCount.keys()).sort()
     const sortedValues = Array.from(yearCount.values()).sort()
@@ -33,10 +41,6 @@ function createTimeline(data) {
     const scale = d3.scalePoint()
         .domain(sortedYears)
         .range([height - margin.bottom, 0])
-
-    // const scale = d3.scaleTime()
-    //     .domain(sortedYears)
-    //     .range([height - margin.bottom, 0])
 
     const minMaxScale = d3.scaleLinear()
         .domain([d3.min(sortedValues), d3.max(sortedValues)])
@@ -57,27 +61,42 @@ function createTimeline(data) {
     let circles = svg.selectAll('circles')
         .data(yearCount);
 
-    circles.join('circle')
+    thinCircle = circles.join('circle')
         .attr('cx', x)
         .attr('cy', d => scale(d[0]))
         .attr('r', d => minMaxScale(d[1]))
+        .attr('id', (d, i) => 'circleID-' + i)
         .style('fill', 'none')
         .style('stroke', 'white')
         .style('stroke-width', '1px')
         .style('stroke-opacity', '0.5')
+
+    circles.join('circle')
+        .attr('cx', x)
+        .attr('cy', d => scale(d[0]))
+        .attr('r', d => minMaxScale(d[1]))
+        .attr('id', (d, i) => 'thickCircleID-' + i)
+        .style('fill', 'none')
+        .style('stroke', 'black')
+        .style('stroke-width', '15px')
+        .style('stroke-opacity', '0')
         .on("mouseover", mouseOver)
         .on("click", mouseClick)
         .on("mouseout", mouseOut)
 
-    circles.join('text')
+    circleText = circles.join('text')
         .attr('x', x)
         .attr('y', d => scale(d[0]))
+        .attr('id', (d, i) => 'textID-' + i)
         .text(d => d[0])
         .style('fill', '#fff')
         .style("font-size", 12)
         .style("font-family", 'Lekton')
         .style("alignment-baseline", 'central')
         .style("text-anchor", 'middle')
+        .on("mouseover", mouseOver)
+        .on("click", mouseClick)
+        .on("mouseout", mouseOut)
 
     const tooltip = d3.select("#timeline")
         .append("div")
@@ -90,37 +109,31 @@ function createTimeline(data) {
 }
 
 function mouseOver() {
-    d3.select(this)
+    let numberId = d3.select(this).attr('id').split('-')[1]
+    let textID = d3.select(`#textID-${numberId}`)
+        .style('fill', 'red')
+    let circleId = d3.select(`#circleID-${numberId}`)
         .style('stroke', 'red')
-        .append("title")
-        .text(d => `${d[1]} albums were released in ${d[0]}`)
+
+    // d3.select(this)
+    //     .append("title")
+    //     .text(d => `${d[1]} albums were released in ${d[0]}`)
+
+    //tooltip
+    // https://github.com/Caged/d3-tip/blob/HEAD/docs/index.md
+
 }
 
 function mouseOut() {
-    d3.select(this)
+    let numberId = d3.select(this).attr('id').split('-')[1]
+    let textID = d3.select(`#textID-${numberId}`)
+        .style('fill', 'white')
+    let circleId = d3.select(`#circleID-${numberId}`)
         .style('stroke', 'white')
 }
 
 function mouseClick() {
     const str = this.querySelector('title').innerHTML.split(' ')
     const year = str[str.length - 1]
-
-    //neet to use the 'year' filter above to map the albums released on that year.
+    //need to use the 'year' filter above to map the albums released on that year.
 }
-
-try{
-    let testYear = ['2010']
-    const dataArray = Object.entries(data)
-    const albumsPerYear = data.filter(([key, value]) => value.bandAlbums[0].albumYear == "2010")
-    const filteredAlbums = Object.fromEntries(albumsPerYear)
-
-    console.log(filteredAlbums)
-
-}catch(error){
-    console.log('ERROR: ' + error)
-}
-
-
-
-//tooltip
-// https://github.com/Caged/d3-tip/blob/HEAD/docs/index.md
